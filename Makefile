79f4a319bc74d7eefccb206ad7be28fb2e68bbd4
Joao Sallaberry <joaovictorhs@gmail.com>
2014-05-18 23:02:07 +0200

    No Change first version - did not pass

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 %: %.cpp
-	g++ $< -lm -lcrypt -O2 -pipe -DONLINE_JUDGE -o $@
+	g++ $< -lm -lcrypt -O2 -pipe -DONLINE_JUDGE -std=c++11 -o $@
---
+++ no_change.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> coins;
+
+bool can_pay(int value, int coin, int can_use) {
+    //no more coin types left
+    if (coin >= coins.size())
+	return false;
+    
+    // can pay with this coin
+    int coin_value = coins[coin];
+    if (value%coin_value == 0 && can_use >= value/coin_value)
+	return true;
+
+    // pay the rest with other coins
+    int max_use = min(value/coin_value, can_use) ;
+    for ( ; max_use > 0; max_use--) {
+	if (can_pay(value - max_use * coin_value, coin + 1, max_use))
+	    return true;
+    }
+    
+    return false;
+}
+
+int main () {
+    int cases;
+    int value, coin_types;
+
+    cin >> cases;
+    for( ; cases > 0; cases--) {
+	cin >> value >> coin_types;
+	
+	for ( ; coin_types > 0; coin_types--) {
+	    int tmp;
+	    cin >> tmp;
+	    coins.push_back(tmp);
+	}
+
+	if(can_pay(value, 0, 100000))
+	    cout << "YES" << endl;
+	else
+	    cout << "NO" << endl;
+	cout << endl;
+	
+    }
+}
