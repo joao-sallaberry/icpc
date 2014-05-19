d7e2b6e92c4b0786b97a203ad1ace949c94e3cab
Joao Sallaberry <joaovictorhs@gmail.com>
2014-05-19 16:12:30 +0200

    mice and maze - finished but not working

--- mice_and_maze.cpp
+++ mice_and_maze.cpp
@@ -1,39 +1,50 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <set>
 
 using namespace std;
+
+#define _DEBUG
 
 #define CELL_MAX 101
 
 unsigned int cells, exit_cell, timer;
 unsigned int dist[CELL_MAX][CELL_MAX];
-unsigned int min_dist[CELL_MAX];
+
 
 unsigned int dijkstra(unsigned int entry) {
+  unsigned int min_dist[CELL_MAX];
+
+  memset(min_dist, -1, CELL_MAX);
   min_dist[entry] = 0;
 
-  vector<unsigned int> cells_not_checked;
+  // group of unvisited cells
+  set<unsigned int> cells_not_visited;
   for (unsigned int i = 1; i < cells; i++)
-    cells_not_checked.push_back(i);
+    cells_not_visited.insert(i);
 
-  for (unsigned int i = 1; i < cells; i++)
-    if (dist[entry][i] < min_dist[i])
-      min_dist[i] = dist[entry][i];
-
-  while() {
-    // get shortest node
-    unsigned int shortest_cell = 0, shortest_distance = -1;
+  while (1) {
+    // get nearest unvisited cell
+    unsigned int nearest_cell = 0, nearest_distance = -1;
     for (unsigned int i = 1; i < cells; i++)
-      if(min_dist[i] < shortest_distance) {
-        shortest_cell = i;
-        shortest_distance = dist[entry][i];
+      if (min_dist[i] < nearest_distance &&
+          cells_not_visited.find(i) != cells_not_visited.end()) {
+        nearest_cell = i;
+        nearest_distance = min_dist[i];
       }
+    if (nearest_cell == exit_cell) {
+#ifdef _DEBUG
+      cout << "rat in cell " << entry << " : " << nearest_distance << endl;
+#endif
+      return nearest_distance;
+    }
 
+    // visit this cell
     for (unsigned int i = 1; i < cells; i++)
-      if (dist[shortest_cell][i] < min_dist[i])
-        min_dist[i] = dist[entry][i];
-
+      if (min_dist[nearest_cell] + dist[nearest_cell][i] < min_dist[i])
+        min_dist[i] = min_dist[nearest_cell] + dist[nearest_cell][i];
+    cells_not_visited.erase(nearest_cell);
 
   }
 
@@ -48,8 +59,7 @@ int main() {
     unsigned int connections;
     cin >> cells >> exit_cell >> timer >> connections;
 
-    memset(dist, -1, CELL_MAX*CELL_MAX); // set infinity distance
-    memset(min_dist, -1, CELL_MAX);
+    memset(dist, -1, CELL_MAX*CELL_MAX); // set infinite distance
 
     for ( ; connections > 0; connections--) {
       unsigned int a, b, distance;
