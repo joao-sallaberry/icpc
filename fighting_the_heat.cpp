03339e3073741fbaa5915acd233485a2a06f258c
Joao Sallaberry <joaovictorhs@gmail.com>
2014-06-23 11:47:57 +0200

    time exceeded

--- fighting_the_heat.cpp
+++ fighting_the_heat.cpp
@@ -2,16 +2,29 @@
 #include <vector>
 #include <cstring>
 #include <string>
+#include <utility> 
 
 using namespace std;
 
 #define MAX_DIM 40
 
+class line {
+public:
+  line() {}
+
+  void append(char c, int x, int y) {
+    str.append(string(1, c));
+    pos.push_back(pair<int,int>(x,y));
+  }
+
+  string str;
+  vector<pair<int, int>> pos;
+};
+
+
 vector<string> words;
-bool table[40][40];
-string rows[MAX_DIM];
-string columns[MAX_DIM];
-string diags[MAX_DIM];
+char table[MAX_DIM][MAX_DIM];
+vector<line> lines;
 
 
 int main() {
@@ -27,65 +40,91 @@ int main() {
       string word;
       cin >> word;
       words.push_back(word);
-      //tekst = string ( tekst.rbegin(), tekst.rend() );
+      word = string (word.rbegin(), word.rend());
+      words.push_back(word);
     }
 
-    //memset(rows, 0, sizeof rows);
-    //memset(columns, 0, sizeof columns);
-    memset(table, false, sizeof table);
+    // table
+    for (int i = 0; i < r; i++) {
+      string s;
+      cin >> s;
+      for (int j = 0; j < c; j++) {
+	//l.append(s.at(j), i, j);
+	//lines.push_back(s);
+	table[i][j] = s.at(j);
+      }
+    }
 
     // rows
-    for (int i = 0; i < r; i++)
-      cin >> rows[i];
+    for (int i = 0; i < r; i++) {
+      line l;
+      for (int j = 0; j < c; j++)
+	l.append(table[i][j], i, j);
+      lines.push_back(l);
+    }
 
     // columns
-    for (int i = 0; i < c; i++)
-      for (int j = 0; j < r; j++) {
-	columns[i].append(string(1, rows[j].at(i)));
-      }
+    for (int i = 0; i < c; i++) {
+      line l;
+      for (int j = 0; j < r; j++)
+        l.append(table[j][i], j, i);
+      lines.push_back(l);
+    }
 
     // diags
-    for (int i = 0; i < r; i++)
-      for (int x = i, y = 0; x < r && y < c; x++, y++) {
-	diags[i].append(string(1, rows[x].at(y)));
-      }
-    for (int i = 1; i < c; i++)
-      for (int x = 0, y = i; x < r && y < c; x++, y++) {
-	diags[r+i-1].append(string(1, rows[x].at(y)));
-      }
-      
-    
+    for (int i = 0; i < r; i++) {
+      line l;
+      for (int x = i, y = 0; x < r && y < c; x++, y++)
+	l.append(table[x][y], x, y);
+      lines.push_back(l);
+    }
+    for (int i = 1; i < c; i++) {
+      line l;
+      for (int x = 0, y = i; x < r && y < c; x++, y++)
+ 	l.append(table[x][y], x, y);
+      lines.push_back(l);
+    }
 
-    //for (int i = 0; i < r+c-1; i++)
-    //cout << diags[i] << endl;
+    // opp diags
+    for (int i = 0; i < r; i++) {
+      line l;
+      for (int x = i, y = c-1; x < r && y >= 0; x++, y--)
+	l.append(table[x][y], x, y);
+      lines.push_back(l);
+    }
+    for (int i = c-1-1; i >= 0; i--) {
+      line l;
+      for (int x = 0, y = i; x < r && y >= 0; x++, y--)
+ 	l.append(table[x][y], x, y);
+      lines.push_back(l);
+    }
 
-    //cout << columns[5] << endl;
-
-    for (int i = 0; i < r; i++)
-      for (auto a : words) {
+    // search lines
+    for (auto l : lines)
+      for (auto w : words) {
 	size_t pos = 0;
 	while (1) {
-	  pos = rows[i].find(a, pos);
+	  pos = l.str.find(w, pos);
 	  if (pos == string::npos)
 	    break;
-	  for (int j = 0; j < a.length(); j++) {
-	    table[i][pos+j] = true;
-	    //cout << "score1" << endl;
+	  for (int j = 0; j < w.length(); j++) {
+	    table[get<0>(l.pos[pos+j])] [get<1>(l.pos[pos+j])] = 0;
 	  }
-	  if (pos < c-1) pos++;
+	  if (pos < l.str.length()-1) pos++;
 	  else break;
 	}
       }
-
+    
+    //for (auto a : lines)
+    //cout << a.str << endl;
 
     // print
     for (int i = 0; i < r; i++)
       for (int j = 0; j < c; j++)
-	//cout << table[i][j];
-    if (!table[i][j])
-	cout << rows[i].at(j);
+	if (table[i][j])
+	  cout << table[i][j];
     cout << endl;
-
+    
     r = 0;
   }
 
